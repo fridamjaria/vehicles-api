d295cf7c8bf589816d7e94cb143f7c959d1ffbe7
Frida Mjaria <frida.mjaria@prodigyfinance.com>
2020-07-19 18:35:20 +0200

    Initial Commit

---
+++ Vehicles.API/Controllers/BussesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vehicles.API.Controllers
+{
+    [ApiController]
+    [Route("api/busses")]
+    public class BussesController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetBusses()
+        {
+            return new JsonResult("Ok");
+        }
+
+        [HttpGet("{busId:guid}")]
+        public IActionResult GetBus(Guid busId)
+        {
+            return new JsonResult("Ok");
+        }
+
+        [HttpPatch("{busId:guid}")]
+        public ActionResult UpdateBus(Guid busId, JsonOptions options)
+        {
+            if (options == null)
+            {
+                return BadRequest();
+            }
+
+            return new JsonResult("Updated");
+        }
+
+        [HttpPost]
+        public ActionResult CreateBus(JsonOptions options)
+        {
+            if (options == null)
+            {
+                return BadRequest();
+            }
+
+            return new JsonResult("Created");
+        }
+
+        [HttpDelete("{busId:guid}")]
+        public ActionResult DeleteBus(Guid busId)
+        {
+            return new JsonResult("Deleted");
+        }
+    }
+}
---
+++ Vehicles.API/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vehicles.API.Controllers
+{
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/health")]
+        public IActionResult GetHealth()
+        {
+            return new JsonResult("Ok");
+        }
+    }
+}
