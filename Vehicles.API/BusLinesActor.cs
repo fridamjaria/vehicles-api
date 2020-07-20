40b25d5e1aeeb07f66b86e3400259f6ebca8beeb
Frida Mjaria <frida.mjaria@prodigyfinance.com>
2020-07-20 17:19:38 +0200

    Add receive method to call api client actor

--- Vehicles.API/BusLinesActor.cs
+++ Vehicles.API/BusLinesActor.cs
@@ -1,10 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Akka.Actor;
+
 namespace Vehicles.API
 {
-    public class LinesActor
+    public class BusLinesActor : ReceiveActor
     {
-        public LinesActor()
+        public const string startCommand = "start";
+
+        /* The Agency being used is Algoa Bus Company.
+         * From the list of agencies returned, there is no agency
+         * for MyCiTi. For the purpose of this exercise I've defaulted
+         * to using Algoa Bus Company agency.
+         */
+        private const string _agency = "_UxoeIJ20UqKSKfLAP-bXw";
+
+        public BusLinesActor()
         {
+            Receive<string>(message =>
+            {
+                if (string.Equals(message, startCommand, StringComparison.OrdinalIgnoreCase))
+                {
+                    HttpRequestMessage getLinesRequest =
+                    new HttpRequestMessage(HttpMethod.Get, $"/lines?agencies={_agency}");
+
+                    Props ApiClientActorProps = Props.Create(() => new ApiClientActor());
+                    var apiClientActor = Context.ActorOf(ApiClientActorProps);
+
+                    apiClientActor.Tell(getLinesRequest);
+                }
+            });
+
+            Receive<List<object>>(list =>
+            {
+
+            });
         }
     }
 }
